56c2d079381d709619fdcb2a6cae786b56e8d30b
Corentinvls <corentin.vallois@gmail.com>
2021-03-29 14:55:56 +0200

    merge done

--- main.py
+++ main.py
@@ -58,22 +58,26 @@ def decimalTo64(arr):
 
 
 binary_table = ['1000001', '1000010', '1000011', '1000100', '1000101']
-def exo_3(binary_table):
+
+
+def convertASCIItoBinary(binary_table):
+    """exo3 TODO fix ça """
     decimal = 0
     for element in binary_table:
         decimal = decimal * 2 + int(element)
         print(f"The decimal value is: {decimal}")
 
 
-numbers_table = [16,20,9,3,17,4,20]
-def exo_8(numbers_table):
+numbers_table = [16, 20, 9, 3, 17, 4, 20]
+
+
+def convertBinaryToDecimal(numbers_table):
+    """exo8 TODO fix ça """
     new_table = []
     for number in numbers_table:
         new_table.append(float(number))
     print(f'number table {new_table}')
 
 
-
 if __name__ == '__main__':
     main()
-
