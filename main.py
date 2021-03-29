482893656b51e12d9d6ba698da5b5856d613af85
DogSobre <juanfernandoherrada13@gmail.com>
2021-03-29 15:30:32 +0200

    exo 4

--- main.py
+++ main.py
@@ -4,17 +4,24 @@ Python Docstring
 """
 
 
+def sorting(A):
+    for i in range(1, len(A)):
+        temporaire = A[i]
+        j = i - 1
+        while j >= 0 and A[j] > temporaire:
+            A[j + 1] = A[j]
+            j -= 1
+        A[j + 1] = temporaire
+    return A
+
+
 def main():
     """The main
     """
 
     print('hello world')
-
-
-A = '1000001'
-
-def sorting(A):
-    return(A)
+    arr = sorting(list('1000001','1000010','1000011','1000100','1000101'))
+    print(arr)
 
 
 if __name__ == '__main__':
