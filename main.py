49602bc9830a47a43bee4f084d1df14b6c35f0f7
DebailleuxJonathan <jonathan.debailleux@edu.itescia.fr>
2021-03-29 14:58:18 +0200

    exo

--- main.py
+++ main.py
@@ -10,5 +10,21 @@ def main():
     print('hello world')
 
 
+def exo2():
+    mytab = [65, 66, 67, 68, 69]
+    for i in mytab:
+        print(chr(i))
+
+
+def exo6():
+    mylist = '0100000101000010010000110100010001000101'
+    # for i in mylist.split():
+    #     print(list(i))
+    length = len(mylist)
+    split = length//6
+    result = mylist[:split]
+    print(result)
+
+
 if __name__ == '__main__':
-    main()
+    exo6()
